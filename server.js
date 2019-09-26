7c52299af3f64c780b25f14a0b2e6e86464b0ad4
Anunciacao <renan.anunciacao@capgemini.com>
2019-09-26 11:34:46 -0300

    Criação do primeiro model utilizando o Mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 //Iniciando o app
 const app = express()
@@ -12,9 +13,19 @@ mongoose.connect('mongodb://localhost:27017/nodeapi',
     }
 )
 
+requireDir("./src/models")
+
+const Product = mongoose.model('Product')
+
 //Criação de rota
 app.get("/", (req, res) => {
-    res.send('Hello World')
+    Product.create({
+        title: 'React Native',
+        description: 'Learn build native apps with React',
+        url: 'https://github.com/facebook/react-native'
+    })
+    
+    return res.send('Hello World')
 })
 
 app.listen('3000')
