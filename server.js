32f17848786d55d0e3a9ebeacc26feca05be1c90
Anunciacao <renan.anunciacao@capgemini.com>
2019-09-26 10:52:13 -0300

    Instalação do mongoose e conexão com o BD

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
+//Iniciando o app
 const app = express()
 
+//Conexão com o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', 
+    {
+        useNewUrlParser: true,  
+        useUnifiedTopology: true
+    }
+)
+
+//Criação de rota
 app.get("/", (req, res) => {
     res.send('Hello World')
 })
